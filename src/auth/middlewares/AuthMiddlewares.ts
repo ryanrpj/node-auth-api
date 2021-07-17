4373686cb8792089150b136b91c92e1b1041fd16
Esan <esandeveloper@protonmail.com>
2021-07-17 16:08:33 -0300

    made async auth middleware synchronous

--- src/auth/middlewares/AuthMiddlewares.ts
+++ src/auth/middlewares/AuthMiddlewares.ts
@@ -6,24 +6,22 @@ import { Strings } from '../../common/constants';
 import { HttpStatusCode } from '../../common/enums';
 
 export default class AuthMiddlewares {
-    public static async checkIfUserIsAuthenticated(req: Request, res: Response, next: NextFunction): Promise<void> {
-        const {authorization} = req.headers;
+    public static checkIfUserIsAuthenticated(req: Request, res: Response, next: NextFunction): void {
+        const {authorization} = req.headers || {};
 
-        if(!authorization) return next(new AuthException({
-            errorMessage: Strings.error.UNAUTHORIZED, statusCode: HttpStatusCode.UNAUTHORIZED
-        }));
+        if(!authorization) throw new AuthException({errorMessage: Strings.error.UNAUTHORIZED, statusCode: HttpStatusCode.UNAUTHORIZED});
 
         const splittedHeader = authorization.split(' ');
 
-        if(splittedHeader[0] !== 'Bearer') return next(new InvalidInputException({errorMessage: Strings.error.NOT_BEARER_HEADER}));
-        if(!splittedHeader[1]) return next(new InvalidInputException({errorMessage: Strings.error.MISSING_AUTH_TOKEN}));
+        if(splittedHeader[0] !== 'Bearer') throw new InvalidInputException({errorMessage: Strings.error.NOT_BEARER_HEADER});
+        if(!splittedHeader[1]) throw new InvalidInputException({errorMessage: Strings.error.MISSING_AUTH_TOKEN});
 
         try {
             const result = jwt.verify(splittedHeader[1], process.env.JWT_SECRET!);
             req.body.user = result;
             next();
         } catch {
-            next(new InvalidInputException({errorMessage: Strings.error.INVALID_AUTH_TOKEN}));
+            throw new InvalidInputException({errorMessage: Strings.error.INVALID_AUTH_TOKEN});
         }
     }
 
